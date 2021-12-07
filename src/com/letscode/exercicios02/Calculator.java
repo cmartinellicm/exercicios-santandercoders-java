30763e4e016c7ee28b3d9d77a621f1bbe9a78120
Cássia Martinelli <56940890+cmartinellicm@users.noreply.github.com>
2021-12-07 20:30:42 -0300

    Calculator: four operations working

--- src/com/letscode/exercicios02/Calculator.java
+++ src/com/letscode/exercicios02/Calculator.java
@@ -8,27 +8,27 @@ public class Calculator {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Select an operation: 1-Sum 2-Subtraction 3-Multiplication 4-Division");
+        System.out.print("Select an operation (1-Sum 2-Subtraction 3-Multiplication 4-Division): ");
         int operation = scan.nextInt();
 
-        System.out.print("Type first number: ");
+        System.out.print("Type first integer number: ");
         int firstNumber = scan.nextInt();
 
-        System.out.print("Type second number: ");
+        System.out.print("Type second integer number: ");
         int secondNumber = scan.nextInt();
 
         switch (operation) {
             case 1:
-                System.out.printf("Result = %d", sum(firstNumber, secondNumber));
+                System.out.printf("%d + %d = %d", firstNumber, secondNumber, sum(firstNumber, secondNumber));
                 break;
             case 2:
-                System.out.println("Subtraction");
+                System.out.printf("%d - %d = %d", firstNumber, secondNumber, subtract(firstNumber, secondNumber));
                 break;
             case 3:
-                System.out.println("Multiplication");
+                System.out.printf("%d x %d = %d", firstNumber, secondNumber, multiply(firstNumber, secondNumber));
                 break;
             case 4:
-                System.out.println("Division");
+                System.out.printf("%d / %d = %.2f", firstNumber, secondNumber, divide(firstNumber, secondNumber));
                 break;
             default:
                 System.out.println("Invalid operation!");
@@ -38,5 +38,17 @@ public class Calculator {
 
     public static int sum(int firstNumber, int secondNumber) {
         return firstNumber + secondNumber;
+    }
+
+    public static int subtract(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public static int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public static double divide(float firstNumber, float secondNumber) {
+        return firstNumber / secondNumber;
     }
 }
