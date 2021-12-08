11ca6780542fb05bdc54f3aa6e52d6b5ac7a9afa
Cássia Martinelli <56940890+cmartinellicm@users.noreply.github.com>
2021-12-07 22:11:28 -0300

    Calculator: created validations

--- src/com/letscode/exercicios02/Calculator.java
+++ src/com/letscode/exercicios02/Calculator.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class Calculator {
 
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Select an operation (1-Sum 2-Subtraction 3-Multiplication 4-Division): ");
         int operation = scan.nextInt();
 
+        if (operation < 0 || operation > 4) {
+            System.out.println("Invalid operation!");
+            System.exit(0);
+        }
+
         System.out.print("Type first integer number: ");
         int firstNumber = scan.nextInt();
-
         System.out.print("Type second integer number: ");
         int secondNumber = scan.nextInt();
 
@@ -28,10 +31,13 @@ public class Calculator {
                 System.out.printf("%d x %d = %d", firstNumber, secondNumber, multiply(firstNumber, secondNumber));
                 break;
             case 4:
+                if (secondNumber == 0) {
+                    System.out.println("Can't divide by zero!");
+                    System.exit(0);
+                }
                 System.out.printf("%d / %d = %.2f", firstNumber, secondNumber, divide(firstNumber, secondNumber));
                 break;
             default:
-                System.out.println("Invalid operation!");
                 break;
         }
     }
